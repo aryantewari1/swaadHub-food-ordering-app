68cd3bbf5d3c35b15c6d3b36c1c1796db04ce818
Aryan Tewari <aryantewari308@gmail.com>
2024-09-20 01:07:21 +0530

    location.js and improved the UI of header via css

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,19 +11,21 @@ const Header = function () {
   const cartItems = useSelector((store) => store.cart.items);
 
   return (
-    <nav className="flex justify-around items-center shadow-[0_2px_25px_2px_rgb(0,0,0,0.0.07)] font-['montserrat'] font-medium">
+    <nav className="flex justify-around items-center shadow-[0_2px_25px_2px_rgb(0,0,0,0.0.07)] font-['montserrat'] font-medium h-20">
       <div className="flex items-center flex-[1] h-full ml-28">
-        <img
+        {/* <img
           className="w-28 h-full object-cover overflow-hidden"
           src={LOGO_URL}
           alt="website-logo"
-        />
+        /> */}
         <Link to="/">
-          <p className="font-['poppins'] text-[20px] font-semibold">swaadHub</p>
+          <p className="font-['poppins'] text-[20px] font-semibold hover:text-lg">
+            swaadHub
+          </p>
         </Link>
       </div>
 
-      <div className="flex-[2] max-w-[900px]">
+      <div className="flex-[2] max-w-[700px]">
         <ul className="flex justify-evenly">
           <li>Online status: {online ? "🟢" : "🔴"}</li>
           <li>
@@ -32,21 +34,25 @@ const Header = function () {
               onClick={() => {
                 setFilteredList(listOfRestaurants);
               }}
+              className="hover:font-bold hover:text-orange-500"
             >
               Home
             </Link>
           </li>
           <li>
-            <Link to="/contact">Contact us</Link>
+            <Link to="/about" className="hover:font-bold hover:text-orange-500">
+              About us
+            </Link>
           </li>
           <li>
-            <Link to="/about">About us</Link>
-          </li>
-          <li>
-            <Link to="/order">Order</Link>
+            <Link to="/order" className="hover:font-bold hover:text-orange-500">
+              Order
+            </Link>
           </li>
           <li className="font-bold">
-            <Link to="/cart">Cart-({cartItems.length + " items"})</Link>
+            <Link to="/cart" className="hover:font-bold hover:text-orange-500">
+              Cart-({cartItems.length + " items"})
+            </Link>
           </li>
         </ul>
       </div>
---
+++ src/utils/location.js
@@ -0,0 +1,9 @@
+const location = {
+  kanpur: "lat=26.4122962&lng=80.3207884",
+  Bangalore: "lat=12.9352403&lng=77.624532",
+  Hyderabad: "lat=18.1124372&lng=79.01929969999999",
+  Ghaziabad: "lat=28.6588739&lng=77.4795184",
+  Delhi: "lat=28.6304203&lng=77.21772159999999",
+  Mumbai: "lat=19.0635747&lng=72.84047029999999",
+};
+export default location;
